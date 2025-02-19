5f436eccb13f0ade49dae344444b467c6c56875b
Tomas Gonzalez <tpg3@njit.edu>
2025-02-19 11:40:03 -0500

    added post api

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,29 @@
 # User-related endpoints
 from fastapi import APIRouter, Depends
+import jwt
 
 router = APIRouter()
 
 @router.get("/users")
 def get_users():
     return {"message": "List of users"}
-# TODO: POST /users/ - Create user
+
+
+@router.post("/users")
+def create_access_token(data: dict):
+    return dict
+
+###potential code for generating jwt###
+    #expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    
+    # Create the JWT token
+    #token = jwt.encode(
+        #{'exp': expiration_time, **data},  # Add the expiration to the data
+        #SECRET_KEY,
+        #algorithm='HS256'
+    #)
+###
+
 # TODO: GET /users/{user_id} - Get user
 # TODO: PUT /users/{user_id} - Update user
 # TODO: DELETE /users/{user_id} - Delete user 
