5a979d6c4441daf0c6130558326913e7c566735e
dhanesh-kh <khemrajdhanesh@gmail.com>
2025-02-19 20:37:30 -0500

    revert main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # Main FastAPI application entry point
-from routers import users, auth
+from routers import users
 from fastapi import FastAPI
 
 #initializes fastAPI
@@ -9,21 +9,12 @@ app = FastAPI(
     version="1.0.0"
 )
 
-#initializes database
-"""
-@app.on_event("startup")
-async def startup():
-    Database.initialize(settings.database_url)
-"""
-
-
 #root api. base backend is hit. it returns this message.
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI app! This is a CI/CD test."}
 
 app.include_router(users.router)
-app.include_router(auth.router)
 
 # TODO: Include routers
 # TODO: Add middleware
