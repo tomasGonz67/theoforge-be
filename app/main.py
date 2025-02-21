a52f3b12669d7226e4c0940f6157d279a77c84b7
Tomas Gonzalez <tpg3@njit.edu>
2025-02-20 20:13:08 -0500

    TESTGIN

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 # Main FastAPI application entry point
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import users, auth
 from fastapi import FastAPI
 from app.database import Database
@@ -28,6 +29,14 @@ def read_root():
 
 app.include_router(users.router)
 app.include_router(auth.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify the origins you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # TODO: Include routers
 # TODO: Add middleware
