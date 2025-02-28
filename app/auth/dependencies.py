9a394122592f5b4fb6f29370bec8b5927175b355
dmg52 <dmg52@njit.edu>
2025-02-27 19:37:54 -0500

    Edited auth/dependencies.py for retrieving user access_token. Added addtional files for JWT: token_schema, jwt_service, settings/config

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -2,6 +2,8 @@ from builtins import Exception
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import Database
+from jose import JWTError
+from app.services.jwt_service import decode_token
 
 async def get_db() -> AsyncSession:
     """Dependency that provides a database session for each request."""
@@ -14,3 +16,17 @@ async def get_db() -> AsyncSession:
             raise HTTPException(status_code=500, detail=str(e))
         finally:
             await session.close()
+
+# Retrieve current user based on access_token via cookie
+def get_current_user(access_token: str = Cookie(None)):
+    if access_token is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+    try:
+        payload = decode_token(access_token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return username
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
