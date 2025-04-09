c5e9d275f6892f0c2930157e25bce8c4d12b41e9
Tomas Gonzalez <tpg3@njit.edu>
2025-04-09 01:24:53 -0400

    test again

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app.include_router(guest.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return {"message": "Hello World again!"}
 
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
