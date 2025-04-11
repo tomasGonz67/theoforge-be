b4ebd24574dce298caadde99fe662a02a875af44
Tomas Gonzalez <tpg3@njit.edu>
2025-04-11 19:19:08 -0400

    testing the fixes on the pipeline

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app.include_router(guest.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World again!"}
+    return {"another test!"}
 
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
