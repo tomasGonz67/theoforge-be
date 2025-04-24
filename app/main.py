fefbef575c451cd2976da6f6bf6b8d4f24b5b881
Tomas Gonzalez <tpg3@njit.edu>
2025-04-24 14:29:23 -0400

    hello monkeys

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app.include_router(guest.router)
 
 @app.get("/")
 async def root():
-    return {"hello world!"}
+    return {"HELLO MONKEYS!"}
 
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
