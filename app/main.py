bad99a4cd37e13f10a2dc4a68a36b066d657a5a8
Tomas Gonzalez <tpg3@njit.edu>
2025-04-24 14:58:46 -0400

    forgot its case sensitive

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(guest.router)
 
-@app.get("/api")
+@app.get("/API/")
 async def root():
     return {"message":"HELLO MONKEYS!"}
 
